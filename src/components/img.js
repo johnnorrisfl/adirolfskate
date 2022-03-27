572779447dcab3cedb0ebf5c591301b5c66af65c
johnnorrisfl <adirolfskateboarding@gmail.com>
2022-03-27 17:15:49 -0400

    Remove image prepended path.

--- src/components/img.js
+++ src/components/img.js
@@ -5,7 +5,7 @@ const regex = /[/]+/g
 const Img = ({ src, alt, ...props }) => {
   const qualifiedSrc = process.env.IMG_PATH
     ? `${process.env.IMG_PATH}${src}`
-    : `/adirolfskate/${src}`
+    : src
   const cleaned = qualifiedSrc.replace(regex, "/")
   return <img className={props?.["class"]} src={cleaned} alt={alt} />
 }

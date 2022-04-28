0feb36eed48bf0edb06e2eb479823b87cf87eca7
johnnorrisfl <adirolfskateboarding@gmail.com>
2022-04-27 21:25:09 -0400

    Add gtag and tracking.

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,35 @@ module.exports = {
     author: "",
     siteUrl: `https://adirolfskate.com`,
   },
-  plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-postcss`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-postcss`,
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-0WZ8HCM82H", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+          // Defaults to https://www.googletagmanager.com
+          origin: "adirolfskate.com",
+        },
+      },
+    },
+  ],
   pathPrefix: "",
 }

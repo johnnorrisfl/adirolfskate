255ef305f738692a4f3504f75ded8cbb39a726ff
John Grant <2171719+JohnGrant@users.noreply.github.com>
2022-06-11 02:37:00 -0400

    Fix up image path.

--- src/components/nav-button.js
+++ src/components/nav-button.js
@@ -4,7 +4,7 @@ const NavButton = ({ onClick, isOpen }) => (
   <div className="flex flex-col justify-center pr-2">
     <button className="border-2 border-adirolf-blue" onClick={onClick}>
       <img
-        src={`${isOpen ? "menu close.svg" : "menu.svg"}`}
+        src={`${isOpen ? "/menu close.svg" : "/menu.svg"}`}
         alt="Main navigation menu"
       />
     </button>
